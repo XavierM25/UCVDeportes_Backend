dfdb8f52f180ff88131334b13d567a01e8a45cee
XavierM25 <xaviermg2504@gmail.com>
2025-05-22 01:49:23 -0500

    eliminar: eliminar interfaces de categorías, subcategorías, credenciales y usuario

--- src/interfaces/categories.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface CategoriesInterface{
-    id: number;
-    nombre_categoria: string;
-    descripcion: string;
-}
--- src/interfaces/subcategories.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface SubCategories {
-    id: number;
-    nombre_subcategoria: string;
-    descripcion: string;
-    id_categoria: number;
-    nombre_categoria: string;
-}
